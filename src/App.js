c835b2b4e19def972a874c8d2c0607f7ce3d8f85
KHAIRUL57 <khairultamimi5757@gmail.com>
2020-09-03 02:31:09 +0700

    learn to create react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Form from "./Component/form/form";
+import Form1 from "./Pages/Form1/form1";
+import Number from "./Pages/Number/Number";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form />
+      <hr />
+      <Form1 />
+      <hr />
+      <Number />
     </div>
   );
 }
